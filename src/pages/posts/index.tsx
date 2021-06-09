ee98ee07d5a385d8fe9720dbbfab7287ef4a0b8d
Luí Matheo <luimatheo@gmail.com>
2021-06-09 12:52:22 -0300

    páginas estáticas

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
@@ -27,11 +28,13 @@ export default function Posts({ posts }: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                        <a key={post.slug} href="">
-                        <time>{post.updateAt}</time>
-                        <strong>{post.title}</strong>
-                        <p>{post.excerpt}</p>
-                    </a>
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updateAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
                     ))}
                 </div>
             </main>
@@ -61,6 +64,7 @@ export const getStaticProps: GetStaticProps = async () => {
             })
         }
     })
+    
     return {
         props: { posts }
     }
